8830be9d8cf2bc057261ce27733ee7b7cb50f296
Aasim Jadaf <jadaf2016@.com>
2026-03-03 17:06:06 +0000

    Update version to v3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const VERSION = process.env.VERSION || "v3";
+const VERSION = process.env.VERSION || "v4";
 const ENVIRONMENT = process.env.ENVIRONMENT || "BLUE";
 
 app.get("/", (req, res) => {
