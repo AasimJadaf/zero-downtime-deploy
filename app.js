8a44b22e30e12b251e8257dec258db8246524245
Aasim Jadaf <jadaf2016@.com>
2026-03-04 08:54:34 +0000

    Test auto deploy with status 600

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  res.status(500).json({ status: "OK" });
+  res.status(600).json({ status: "OK" });
 });
 
 app.listen(PORT, () => {
